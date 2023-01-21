dea48fcff04decdc9e0535e2be5079c23fc177ef
ChiaraBrambilla96 <123060385+ChiaraBrambilla96@users.noreply.github.com>
2023-01-21 12:52:18 +0100

    Add files via upload

---
+++ My Jupyter Notebook on IBM Watson Studio.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # My Jupyter Notebook on IBM Watson Studio
+
+# __Chiara Brambilla__ \
+# Data Scientist
+
+# _I am interested in data science because I aim to understand the real world through information. I love to gain new knowledge and keep updated about new technologies. Data Science will allow me to use my skills to achieve this goal._
+
+# ### The following code cell will display the solution of some easy computations.
+
+# In[5]:
+
+
+1+1*(2-5)
+
+
+# In this markdown cell are displayed:
+# - a bulleted list
+# - a numbered list
+# - a table
+# - two hyperlinks
+# 
+# This numbered list contains two hyperlinks
+# 1. [link_1](https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.consigliamidove.it%2Fislanda%2F&psig=AOvVaw2k0efX3Oh7HD767HIfOjPx&ust=1674385882811000&source=images&cd=vfe&ved=0CBAQjRxqFwoTCPiy5s7D2PwCFQAAAAAdAAAAABAJ) 
+# 2. [link_2](https://www.icelandtours.is/) 
+# 
+# The following table explains the content of the hyperlinks above:
+# 
+# | __Number__ | __Content__ |
+# | :-: | --- |
+# | 1 | Webpage about Iceland |
+# | 2 | Webpage for Iceland tours |
